f7fd76ceabd4241abf343fee95b275d26e6cbf1d
Gaby Gaetani <gabrielagaetani@gmail.com>
2017-05-02 09:54:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,22 +4,23 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 var products = require('./routes/products');
 
-var app = express();
-
+// load mongoose package
+var mongoose = require('mongoose');
 
 // Use native Node promises
 mongoose.Promise = global.Promise;
+
 // connect to MongoDB
 mongoose.connect('mongodb://localhost/lista-bd')
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
 
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -44,15 +45,29 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
 
 module.exports = app;
